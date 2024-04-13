13954369f87160f3b3664509579582359d04db69
Ice-Sigil <grayjack04@hotmail.com>
2024-04-13 19:15:36 -0400

    modified:   Shopkeeper.cs

--- Shopkeeper.cs
+++ Shopkeeper.cs
@@ -7,7 +7,11 @@ public class Shopkeeper : NPCInt{
 
     _HP = 100; //Temp, will be changed for game balancing and/or difficulty
 
-
+    _possibleDialogue[0] = "Is this really what you want to do? Fine, then. Die like the rest.";
+    _possibleDialogue[1] = "Fool. You'll get what you deserve.";
+    _possibleDialogue[2] = "You should have never come down here.";
+    _possibleDialogue[3] = "May the gods let you die quickly.";
+    _possibleDialogue[4] = "Wretched filth. I'll put you where you belong.";
 
 
 }
