7527cb71da7cdf1d95dbcf7b6eda7dfa543342d1
Ice-Sigil <grayjack04@hotmail.com>
2024-04-16 10:44:48 -0400

    modified:   Shopkeeper.cs

--- Shopkeeper.cs
+++ Shopkeeper.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using System;   
 namespace StarterGame{
-    public class Shopkeeper : NPCInt{
-        bool _isShopkeeper = true;
+     public class Shopkeeper : NPCInt 
+    {
+        public Shopkeeper(){
+        _isShopkeeper = true;
         _isHuman = true;
         _isHostile = false;
         _discoveryDialogue = "Good to see you. Lots of things for sale here. Take a look.";
 
+        
         _HP = 100; //Temp, will be changed for game balancing and/or difficulty
 
         _possibleDialogue[0] = "Is this really what you want to do? Fine, then. Die like the rest.";
@@ -15,24 +18,18 @@ namespace StarterGame{
         _possibleDialogue[2] = "You should have never come down here.";
         _possibleDialogue[3] = "May the gods let you die quickly.";
         _possibleDialogue[4] = "Wretched filth. I'll put you where you belong.";
-
-        if(_HP < 100){
-            _isHostile = true;
-            sayDialogue();
         }
-
-        if(!_isHostile){
-            showStoreMenu();
-        }
-
-        private string[] inventory = new String[4];
         
-        private randomizeInventory(string[] inventory){
+        public string[] inventory = new String[4];
+        
+        public void randomizeInventory(string[] inventory){
             var rand = new Random();
         }
+
         public void showStoreMenu(Player player){
-            player.InfoMessage("\"Wanna have a look? Sure. This is what I've got in stock.\"
-            \n ");
+            player.InfoMessage("\"Wanna have a look? Sure. This is what I've got in stock.\" \n");
         }
+
     }
+
 }
