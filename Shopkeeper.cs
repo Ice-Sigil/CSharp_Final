d7ee32541bc1e8ccb12739a7d84c1c3c40a1fa31
Ice-Sigil <grayjack04@hotmail.com>
2024-04-16 10:22:25 -0400

    modified:   Shopkeeper.cs

--- Shopkeeper.cs
+++ Shopkeeper.cs
@@ -1,36 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
-using System;
-using StarterGame;
-public class Shopkeeper : NPCInt{
-    _isShopkeeper = true;
-    _isHuman = true;
-    _isHostile = false;
-    _discoveryDialogue = "Good to see you. Lots of things for sale here. Take a look.";
+using System;   
+namespace StarterGame{
+    public class Shopkeeper : NPCInt{
+        bool _isShopkeeper = true;
+        _isHuman = true;
+        _isHostile = false;
+        _discoveryDialogue = "Good to see you. Lots of things for sale here. Take a look.";
 
-    _HP = 100; //Temp, will be changed for game balancing and/or difficulty
+        _HP = 100; //Temp, will be changed for game balancing and/or difficulty
 
-    _possibleDialogue[0] = "Is this really what you want to do? Fine, then. Die like the rest.";
-    _possibleDialogue[1] = "Fool. You'll get what you deserve.";
-    _possibleDialogue[2] = "You should have never come down here.";
-    _possibleDialogue[3] = "May the gods let you die quickly.";
-    _possibleDialogue[4] = "Wretched filth. I'll put you where you belong.";
+        _possibleDialogue[0] = "Is this really what you want to do? Fine, then. Die like the rest.";
+        _possibleDialogue[1] = "Fool. You'll get what you deserve.";
+        _possibleDialogue[2] = "You should have never come down here.";
+        _possibleDialogue[3] = "May the gods let you die quickly.";
+        _possibleDialogue[4] = "Wretched filth. I'll put you where you belong.";
 
-    if(_HP < 100){
-        _isHostile = true;
-        sayDialogue();
-    }
+        if(_HP < 100){
+            _isHostile = true;
+            sayDialogue();
+        }
 
-    if(!_isHostile){
-        showStoreMenu();
-    }
+        if(!_isHostile){
+            showStoreMenu();
+        }
 
-    private string[] inventory = new String[4];
-    private randomizeInventory(string[] inventory){
-        var rand = new Random();
-    }
-    public void showStoreMenu(Player player){
-        player.InfoMessage("\"Wanna have a look? Sure. This is what I've got in stock.\"
-        \n ");
+        private string[] inventory = new String[4];
+        
+        private randomizeInventory(string[] inventory){
+            var rand = new Random();
+        }
+        public void showStoreMenu(Player player){
+            player.InfoMessage("\"Wanna have a look? Sure. This is what I've got in stock.\"
+            \n ");
+        }
     }
 }
