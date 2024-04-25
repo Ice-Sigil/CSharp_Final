9d97a625cd8d1c4ff0946c6d8efc3b2a05aba182
Ace <45800061+aceofdiamonds4500@users.noreply.github.com>
2024-04-25 10:43:13 -0400

    Add files via upload
    **This requires an update on GameInterfaces

--- GameInterfaces.cs
+++ GameInterfaces.cs
@@ -1,7 +1,30 @@
-namespace StarterGame;
-public interface ICharacter{
-    public string? Name { get; set; }
-    public int HP { get; set; } 
-    public int ATK { get; set; } 
-    public int DEF { get; set; } 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarterGame
+{
+    public interface IRoomDelegate
+    {
+        public Room ContainingRoom {get; set;}
+        public Room OnGetExit(Room room);
+        public string OnTag(string tag);
+    }
+
+    public interface IItem
+    {
+        public string Name {get;set;}
+        public float Weight {get;}
+        public string Description {get;}
+        public void Decorate(IItem decorator);
+        public bool IsContainer { get; }
+
+    }
+
+    public interface IItemContainer : IItem
+    {
+        public bool Insert(IItem item);
+        public IItem Remove(string itemName);
+    }
 }
