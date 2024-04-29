364fa236516a475bae263d3d82dcb7f51d83fb20
Ace <45800061+aceofdiamonds4500@users.noreply.github.com>
2024-04-29 16:42:42 -0400

    4-29-24
    Added functionality for Program to ask user if they want to start a new game
    Added Next Command to proceed to the next floor
    Added Next Command to CommandWords
    note: need to edit GameWorld.cs later to implement Next functionality

--- Program.cs
+++ Program.cs
@@ -9,10 +9,35 @@ namespace StarterGame
     {
         static void Main(string[] args)
         {
-            Game game = new Game();
-            game.Start();
-            game.Play();
-            game.End();
+            bool quit = false;
+            while(!quit)
+            {
+                string? input = "";
+                bool valid = false;
+                Game game = new Game();
+                game.Start();
+                game.Play();
+                Console.WriteLine("Start a new game? Y/N");
+                while (!valid)
+                {
+                    input = Console.ReadLine();
+                    if(input.ToLower() == "n")
+                    {
+                        valid = true;
+                        quit = true;
+                    }
+                    else if(input.ToLower() == "y")
+                    {
+                        valid = true;
+                        Console.WriteLine("Starting new game...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input.");
+                    }
+                }   
+                if(quit){ game.End(); }
+            }
         }
     }
 }
