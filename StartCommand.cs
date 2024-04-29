5836012b0d982431d8ad8d3878f95756c19636e4
Dante Smith <dsmith050602@gmail.com>
2024-04-29 18:34:09 -0400

    yes

--- StartCommand.cs
+++ StartCommand.cs
@@ -2,11 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using StarterGame;
 
-namespace CSharp_Final
-{
-    public class StartCommand
-    {
-        
+namespace CSharp_Final{
+    public class StartCommand : Command{
+        public StartCommand() : base()
+        {
+            this.Name = "start";
+        }
+
+        public override bool Execute(Player player)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
