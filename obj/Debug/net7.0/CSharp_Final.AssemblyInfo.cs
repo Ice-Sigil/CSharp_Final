7c34544e166339490af6a04c46f7fce688fe9564
Ice-Sigil <grayjack04@hotmail.com>
2024-05-01 22:04:44 -0400

    modified:   obj/Debug/net7.0/CSharp_Final.AssemblyInfo.cs 	modified:   obj/Debug/net7.0/CSharp_Final.AssemblyInfoInputs.cache

--- obj/Debug/net7.0/CSharp_Final.AssemblyInfo.cs
+++ obj/Debug/net7.0/CSharp_Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp_Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+060a790bf823af8e90dde5b232f620216f5337b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c738f5be2f82e999aff18dd4341d6b35d4c2f25c")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp_Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp_Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
