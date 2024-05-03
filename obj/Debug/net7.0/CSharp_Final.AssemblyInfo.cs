d5d3362719206257629e215ece422d5877d6b676
Dante Smith <dsmith050602@gmail.com>
2024-05-03 03:15:48 -0400

    -

--- obj/Debug/net7.0/CSharp_Final.AssemblyInfo.cs
+++ obj/Debug/net7.0/CSharp_Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp_Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+948e6aeff0381dc297a99fc6efd0ccc09bed78d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cffce701d1f4410a3a5a4460ce756fac423b9e02")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp_Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp_Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
